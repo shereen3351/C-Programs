0914a349d9145378f97aff1523fa0eacebee47fc
shereen3351 <119235015+shereen3351@users.noreply.github.com>
2022-12-15 23:33:09 +0530

    Create table.c

---
+++ table.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main ()
+{
+    int n,i;
+    printf("enter the number upto which you want to print table \n");
+    scanf("%d",&n);
+    for(i=0;i<=n;i++)
+    {
+        printf("%d \n",i);
+    }
+    getch();
+}
